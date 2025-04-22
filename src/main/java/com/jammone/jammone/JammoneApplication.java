f8f04d405905148980bca8df7f74a04329c19e48
Federico Caltabiano <caltabiano.2213914@studenti.uniroma1.it>
2025-04-22 16:27:53 +0200

    Start of the project

---
+++ src/main/java/com/jammone/jammone/JammoneApplication.java
@@ -0,0 +1,13 @@
+package com.jammone.jammone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JammoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JammoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jammone/jammone/JammoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jammone.jammone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JammoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
